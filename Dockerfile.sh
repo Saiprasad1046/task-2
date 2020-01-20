8a61849c3ccc7826ed2c8a414c6b9707369f652e
sai <sai.thukuntla@gmail.com>
2020-01-20 17:43:25 +0530

    Update Dockerfile.sh

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -4,7 +4,7 @@ RUN mkdir /opt/tomcat/
 
 WORKDIR /opt/tomcat
 RUN curl -O https://www-eu.apache.org/dist/tomcat/tomcat-8/v8.5.40/bin/apache-tomcat-8.5.40.tar.gz
-RUN tar xvfz apache-tomcat-8.5.40.tar.gz
+RUN tar xvfz apache*
 RUN mv apache-tomcat-8.5.40/* /opt/tomcat/.
 RUN yum -y install java
 RUN java -version
